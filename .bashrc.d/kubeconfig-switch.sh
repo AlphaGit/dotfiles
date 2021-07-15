8efc308236ac050585f0d71a92256640388af171
Alpha <alphagma@gmail.com>
2021-07-15 17:45:05 -0400

    kubeconfig-switch

--- .bashrc.d/kubeconfig-switch.sh
+++ .bashrc.d/kubeconfig-switch.sh
@@ -1,3 +1,13 @@
 kubeconfig-switch() {
-    export KUBECONFIG=~/.kube/$1.config
+    RED=`tput setaf 1`
+    GREEN=`tput setaf 2`
+    NC=`tput sgr0`
+
+    FILE=~/.kube/$1.config.yaml
+    if [ -f "$FILE" ]; then
+        export KUBECONFIG=~/.kube/$1.config.yaml
+        echo -e "${GREEN}Success:${NC} Using Kube $1 configuration."
+    else
+        echo -e "${ERROR}Error:${NC} Config file $FILE not found!"
+    fi
 }
