4b1aaf8d79f3adc6ff95f41d79533b5b9110f29d
Alpha <alphagma@gmail.com>
2021-12-02 19:48:56 -0500

    Install script now works in MacOS

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
-SCRIPT_LOCATION=$(readlink -f "$0")
-SCRIPT_DIR=$(dirname $SCRIPT_LOCATION)
-ln -sf $SCRIPT_DIR/* $HOME
+SCRIPT_LOCATION=$(dirname "$0")
+SCRIPT_DIR=$(cd $(dirname $SCRIPT_LOCATION); pwd -P)
+# Match all files except . and ..: https://unix.stackexchange.com/a/186219
+ln -sf $SCRIPT_DIR/{*,.[^.],.??*} $HOME
