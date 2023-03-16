25b139b47c946526e1495ac160412c3415d7482e
Alpha <alphagma@gmail.com>
2023-03-16 14:10:40 -0400

    Concat videos helper function

---
+++ functions/concat-videos.sh
@@ -0,0 +1,46 @@
+concat-videos() {
+    # Check if help was requested
+    if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+        echo "Usage: concat-videos <output_file> <input_file1> <input_file2> ..."
+        echo "Concatenates videos into a single video file."
+        echo "The input files will be converted to a common resolution (1920x1080) before concatenation."
+        return 0
+    fi
+
+    OUTPUT_FILE=$1
+    shift
+    INPUT_FILES=($@)
+
+    echo ""
+    echo "Concatenating videos: "
+    for input_file in $INPUT_FILES; do
+        echo "- $input_file"
+    done
+    echo "Output file: $OUTPUT_FILE"
+    echo ""
+
+    BASE_OUTPUT_FILE="${OUTPUT_FILE%.*}"
+    TMP_LIST="$BASE_OUTPUT_FILE.list.tmp"
+    cat /dev/null > $TMP_LIST
+    for file in $INPUT_FILES; do
+        echo -n "Converting $file to common format... "
+        ffmpeg -hide_banner -loglevel error -i $file -s 1920x1080 $file.common.mp4
+        echo $file.common.mp4 >> $TMP_LIST
+        echo "Done."
+    done
+
+    echo -n "Concatenating videos... "
+    ffmpeg -hide_banner -loglevel error -f concat -safe 0 -i $TMP_LIST -c copy $BASE_OUTPUT_FILE.intermediate.mp4
+    echo "Done."
+
+    echo -n "Converting to final format... "
+    ffmpeg -hide_banner -loglevel error -i $BASE_OUTPUT_FILE.intermediate.mp4 $OUTPUT_FILE
+    echo "Done."
+
+    echo -n "Cleaning up..."
+    rm $TMP_LIST $BASE_OUTPUT_FILE.intermediate.mp4
+    for file in $INPUT_FILES; do
+        rm $file.common.mp4
+    done
+    echo "Done."
+}
