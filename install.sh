cd047f105455e48646a7112489e293797587bd6a
Alpha <alphagma@gmail.com>
2023-06-04 13:51:51 -0400

    New install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 SCRIPT_LOCATION=$(dirname "$0")
 SCRIPT_DIR=$(cd $(dirname $SCRIPT_LOCATION); pwd -P)
-# Match all files except . and ..: https://unix.stackexchange.com/a/186219
-ln -sf $SCRIPT_DIR/{*,.[^.],.??*} $HOME
+find $SCRIPT_DIR -maxdepth 1 ! -path "$SCRIPT_DIR/.git" ! -name "install.sh" ! -path "$SCRIPT_DIR/local_bin" -exec sh -c 'ln -sfn "$0" $HOME && echo "Linking $0 → $HOME/$(basename "$0")"' {} \;
+find $SCRIPT_DIR/local_bin -maxdepth 1 ! -exec sh -c 'ln -sfn "$0" /usr/local/bin && echo "Linking $0 → /usr/local/bin/$(basename "$0")"' {} \;
