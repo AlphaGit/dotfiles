4c8fc3c07075b0484f9547cb31e6849dab65ea9e
Alpha <alphagma@gmail.com>
2024-03-24 13:41:12 -0400

    download_icloud_folder
    - Rewrote the download_icloud_folder function to be compatible with bash naming
    - Included auto-invocation if the script was called instead of sourced
    - Added further iTerm settings

--- functions/download-icloud-folder.sh
+++ functions/download-icloud-folder.sh
@@ -1,12 +1,18 @@
-download-icloud-folder() {
+#!/bin/bash
+download_icloud_folder() {
     # Retrieve the first parameter as the folder to download
     local folder="$1"
 
     # Check that the parameter is not empty and that the folder exists
     if [ -z "$folder" ] || [ ! -d "$folder" ]; then
-        echo "The folder does not exist"
+        echo "The folder \"$folder\" does not exist"
         return 1
     fi
 
     find "$folder" -type f -exec brctl download "{}" \;
 }
+
+# Check if the script was invoked directly
+if [ "${BASH_SOURCE[0]}" == "${0}" ]; then
+    download_icloud_folder "$@"
+fi
