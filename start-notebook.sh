39039c94f7beba02cdb662cb263d8bdf9085de24
Alpha <alphagma@gmail.com>
2024-09-26 17:20:04 -0400

    Fix origin for jupyter notebooks

--- start-notebook.sh
+++ start-notebook.sh
@@ -1,1 +1,1 @@
-docker run --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan jupyter/scipy-notebook
+docker run --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan quay.io/jupyter/scipy-notebook
